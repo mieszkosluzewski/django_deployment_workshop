b17c7b7884999b1eb46be6ca54a71364ad99f552
Mieszko Służewski <mieszkosluzewski@gmail.com>
2018-09-14 14:37:51 +0200

    Add sample view

--- django_deployment_workshop/app/views.py
+++ django_deployment_workshop/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+from django.views import View
+
+
+class Time(View):
+
+    def get(self, request):
+        now = datetime.datetime.now()
+        html = "<html><body>It is now %s.</body></html>" % now
+        return HttpResponse(html)
--- django_deployment_workshop/django_deployment_workshop/urls.py
+++ django_deployment_workshop/django_deployment_workshop/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import Time
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Time.as_view(), name='add-exchange-rate'),
 ]
